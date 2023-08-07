370653678ccacc01b29d6476e96166497148d462
heilya <123752915+heilya@users.noreply.github.com>
2023-08-07 20:01:34 +0200

    task 2 js changed

--- src/components/statistics/statistics.js
+++ src/components/statistics/statistics.js
@@ -1,6 +1,7 @@
 export const Statistics = ({ title, stats }) => (
     <section class="statistics">
-  <h2 class="title">Upload stats</h2>
+        {title?(<h2 class="title">{title}</h2>):<></>}
+  
 
   <ul class="stat-list">
 {stats.map(item => (<li class="item">
