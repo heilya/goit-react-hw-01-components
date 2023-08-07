feeeac286cb6f6eaf7e86da6b4a8815a9fd098f7
heilya <123752915+heilya@users.noreply.github.com>
2023-08-07 21:38:36 +0200

    task 4 js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import user from "../user.json";
 import data from "../data.json";
 import friends from "../friends.json";
+import transactions from "../transactions.json";
 
 
 
 import { Profile } from "./profile/profile";
 import { Statistics } from "./statistics/statistics";
 import { FriendList } from "./friends/friendlist";
+import { TransactionHistory } from "./transactions/transactions";
 
 export const App = () => {
   return (
@@ -23,6 +25,7 @@ export const App = () => {
       <Profile {...user} />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/transactions/transactions.js
@@ -0,0 +1,19 @@
+export const TransactionHistory = ({ items }) => (
+    <table class="transaction-history">
+  <thead>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    {items.map(item =>(<tr>
+      <td>{item.type}</td>
+      <td>{item.amount}</td>
+      <td>{item.currency}</td>
+    </tr>))}
+  </tbody>
+</table>
+);
